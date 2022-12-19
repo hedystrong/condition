07631d2d066201b33553158dccb5bccf872b25ad
hedystrong <hhedy7777@gmail.com>
2022-12-19 15:18:39 +0800

    condition

---
+++ cond.js
@@ -0,0 +1,38 @@
+let a = 85;
+let b = 75;
+let c = 96;
+let d = 69;
+
+if(a<80){
+    a=0;
+}
+if(b<80){
+    b=0;
+    }
+if(c<80){
+    c=0;
+    }
+if(d<80){
+    d=0;
+    }
+let sum = (a+b+c+d);
+console.log(sum);
+
+let w = 3;
+let s = 7;
+let y = 2;
+let m = 4;
+if(w>5){
+    a = 1;
+}
+if(s>5){
+    s = 1;
+}
+if(y>5){
+    y = 1;
+}
+if(m>5){
+    m = 1;
+}
+let mul = (w*s*y*m);
+console.log(mul);
